f9fc3f09ffab232d432811e6ebafd6fd56e05978
Abigail Robinson <abbyrobinson@shaw.ca>
2022-10-05 11:07:17 -0600

    added product list, edit user experience

--- repl-cart.js
+++ repl-cart.js
@@ -1,15 +1,42 @@
 import rl from "readline-sync";
 
-let commands = ["list products:"];
+let commands = ["list products", "show product", "add to cart"];
+let products = [
+  {
+    name: "hair spray",
+    price: "$5",
+    description: "keep your hair in place",
+    UPC: "34df359x",
+  },
+  {
+    name: "butter",
+    price: "$4",
+    description: "I can't believe its butter",
+    UPC: "1111abc222",
+  },
+  {
+    name: "bananas",
+    price: "$2",
+    description: "Yellow and yummy",
+    UPC: "4011",
+  },
+  { name: "dog treats", price: "$8", description: "go fetch!", UPC: "3gh5444" },
+];
 
 while (true) {
-  console.log("The commands are: ", commands);
+  console.log("==========================\n The commands are: ", commands);
 
   const theCommand = rl.question("What is your command?: ");
-  console.log(`Hello ${theCommand}`);
 
   if (theCommand === "list products") {
-    console.log("the list of products is ...");
+    console.log("the list of products is: ");
+    products.forEach((product) => {
+      console.log("      - " + product.name);
+    });
+  } else if (theCommand === "show product") {
+    console.log("Poduct details GO HERE");
+  } else if (theCommand === "add the cart") {
+    console.log("added to cart");
   } else {
     console.log(`Invalid command: ${theCommand}`);
   }
