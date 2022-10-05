394e4757b6e301a51e99cac31932ff0b92ebf500
Abigail Robinson <abbyrobinson@shaw.ca>
2022-10-05 10:35:06 -0600

    loop added

--- repl-cart.js
+++ repl-cart.js
@@ -1,12 +1,16 @@
 import rl from "readline-sync";
 
 let commands = ["list products:"];
-console.log("The commands are: ", commands);
-const theCommand = rl.question("What is your command?: ");
-console.log(`Hello ${theCommand}`);
 
-if (theCommand === "list products") {
-  console.log("the list of products is ...");
-} else {
-  console.log("add stuff to cart");
+while (true) {
+  console.log("The commands are: ", commands);
+
+  const theCommand = rl.question("What is your command?: ");
+  console.log(`Hello ${theCommand}`);
+
+  if (theCommand === "list products") {
+    console.log("the list of products is ...");
+  } else {
+    console.log(`Invalid command: ${theCommand}`);
+  }
 }
